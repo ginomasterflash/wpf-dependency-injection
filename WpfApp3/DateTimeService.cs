72cf058af44210a389e2cc2287d851d395e41c7e
Mattia Renna <m.renna@cdshotels.it>
2021-10-23 22:07:49 +0200

    Add project files.

---
+++ WpfApp3/DateTimeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp3
+{
+   public class DateTimeService: IDateTimeService
+    {
+        public string GetDateTimeString() {
+            return DateTime.Today.ToShortDateString();
+        }
+    }
+}
---
+++ WpfApp3/IDateTimeService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp3
+{
+    public interface IDateTimeService
+    {
+        string GetDateTimeString();
+    }
+}
